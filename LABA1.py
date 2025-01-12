d471b8367885c68004c4c74e424db0ca29e0d2a3
Gleb <118745936+MrPerfecttt@users.noreply.github.com>
2025-01-12 22:04:18 +0300

    Add files via upload

---
+++ LABA1.py
@@ -0,0 +1,14 @@
+print("Nomer 1")
+
+print("Введите число")
+a = int(input())
+for i in range(1,a+1):
+    print(i)
+
+print("Nomer 2")
+
+print("Введите первое число")
+b = int(input())
+print("Введите второе число")
+c = int(input())
+print("Большее число:", max(b,c))
